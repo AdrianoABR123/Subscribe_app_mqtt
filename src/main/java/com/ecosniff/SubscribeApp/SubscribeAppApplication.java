f8e557f25f8265c3d415227ffb0bffb73dcffa06
AdrianoABR123 <drianoeloy@gmail.com>
2026-03-01 22:38:16 -0300

    Initial configuration files for the project.

---
+++ src/main/java/com/ecosniff/SubscribeApp/SubscribeAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecosniff.SubscribeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscribeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscribeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecosniff/SubscribeApp/SubscribeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecosniff.SubscribeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscribeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
