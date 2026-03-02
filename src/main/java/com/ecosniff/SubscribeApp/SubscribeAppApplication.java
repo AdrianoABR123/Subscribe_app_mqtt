545a4c6873e04996db44fdcc93b936224f77a651
AdrianoABR123 <drianoeloy@gmail.com>
2026-03-02 00:22:22 -0300

    feat: Initial config for mqtt

--- src/main/java/com/ecosniff/SubscribeApp/SubscribeAppApplication.java
+++ src/main/java/com/ecosniff/SubscribeApp/SubscribeAppApplication.java
@@ -1,13 +1,14 @@
 package com.ecosniff.SubscribeApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SubscribeAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SubscribeAppApplication.class, args);
+		SpringApplication app = new SpringApplication(SubscribeAppApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
 	}
-
 }
